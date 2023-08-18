e3cf1600ba35dd5777ab72964b28dd96b49821c6
s-chance <entropy.tree@outlook.com>
2023-08-19 00:14:35 +0800

    feat:slack文件上传机器人

--- slack-file-bot/main.go
+++ slack-file-bot/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/slack-go/slack"
+	"os"
+)
 
+// slack 文件上传机器人
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-4991956384768-5756949448114-OYOKn9vLnNMtjV3YPTZCmhpt")
+	os.Setenv("CHANNEL_ID", "C04UJMNU5E0")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"1.jpg"}
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL: %s\n", file.Name, file.URL)
+	}
 }
